b6b5fa4ae2a7adafc0b7566c37db0bbbb24e4d67
Al Muhammed Yazeen <59413852+cjmatrix@users.noreply.github.com>
2026-07-26 17:05:40 +0530

    Update redis connection in aiQueue.js

--- backend/queues/aiQueue.js
+++ backend/queues/aiQueue.js
@@ -1,8 +1,9 @@
 import { Queue } from "bullmq";
 import { redisConfig } from "../config/redis.js";
+import { redisConnection } from '../config/redis.js'
 
 
-const aiQueue=new Queue ('ai-queue',{connection:redisConfig});
+const aiQueue=new Queue ('ai-queue',{connection:redisConnection});
 
 export const addAiJob=async(data)=>{
     aiQueue.add('generate-pc',data, {
