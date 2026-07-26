9243ec7ac3324e60f459a57254e2d2660d2627bf
Al Muhammed Yazeen <59413852+cjmatrix@users.noreply.github.com>
2026-07-26 17:06:08 +0530

    Fix redis connection for email queue

--- backend/queues/emailQueue.js
+++ backend/queues/emailQueue.js
@@ -1,8 +1,9 @@
 import { Queue } from 'bullmq';
 import { redisConfig } from '../config/redis.js';
+import { redisConnection } from '../config/redis.js'
 
 
-export const emailQueue = new Queue('email-queue', {connection:redisConfig });
+export const emailQueue = new Queue('email-queue', {connection:redisConnection });
 
 
 export const addEmailJob = async (data) => {
