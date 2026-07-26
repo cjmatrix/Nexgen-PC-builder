8374827077605eaf3e232c62fef10720d0aaec79
Al Muhammed Yazeen <59413852+cjmatrix@users.noreply.github.com>
2026-07-26 18:04:53 +0530

    Adjust cookie security settings for production
    Update cookie settings to use secure and sameSite attributes based on environment.

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -26,17 +26,19 @@ const generateTokens = async (res, userId) => {
 
   await user.save();
 
+  const isProduction = process.env.NODE_ENV === "production";
+
   res.cookie("accessToken", accessToken, {
     httpOnly: true,
-    secure: false,
-    sameSite: "strict",
+    secure: isProduction,
+    sameSite: isProduction ? "none" : "strict",
     maxAge: 10000,
   });
 
   res.cookie("refreshToken", refreshToken, {
     httpOnly: true,
-    secure: false,
-    sameSite: "strict",
+    secure: isProduction,
+    sameSite: isProduction ? "none" : "strict",
     maxAge: 7 * 24 * 60 * 60 * 1000,
   });
 
